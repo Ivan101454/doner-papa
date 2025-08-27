e0b5bbad48513cee7f1b053404585acc00b6f8cf
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-08-27 23:50:14 +0300

    create first controller

---
+++ src/main/java/by/ivan101454/doner_papa/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package by.ivan101454.doner_papa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
