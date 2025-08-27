eec36d22fb158725b73056301ae3a01e447a3343
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-08-27 10:11:32 +0300

    start project

---
+++ src/main/java/by/ivan101454/doner_papa/DonerPapaApplication.java
@@ -0,0 +1,13 @@
+package by.ivan101454.doner_papa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonerPapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonerPapaApplication.class, args);
+	}
+
+}
