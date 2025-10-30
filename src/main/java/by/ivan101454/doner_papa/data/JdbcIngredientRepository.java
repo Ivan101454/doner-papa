194d5d7fd52ff76572124eab5c5230c15cafbc31
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-10-30 22:53:45 +0300

    complete methods jdbc repository

--- src/main/java/by/ivan101454/doner_papa/data/JdbcIngredientRepository.java
+++ src/main/java/by/ivan101454/doner_papa/data/JdbcIngredientRepository.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,16 +18,40 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient
+        );
     }
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        return Optional.empty();
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id
+        );
+        return results.isEmpty() ?
+                Optional.empty() :
+                Optional.of(results.getFirst());
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbcTemplate.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString()
+        );
+        return ingredient;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int i) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type"))
+        );
     }
 }
